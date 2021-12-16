9b197a39e3a5ebdcd452604394aa0914473093b2
alfredbox <alfred.y.box@gmail.com>
2021-12-16 08:32:13 -0800

    Levels plus sensitivity

--- tilt.py
+++ tilt.py
@@ -4,23 +4,28 @@ import math
 from check_goal import check_goal
 from check_wall import check_wall
 from draw import draw_ball, draw_wall, draw_goal
+from levels import levels
 
 sense = SenseHat()
 
-g = (3,2)
-w = [(3,4), (3,3), (2,3)]
-reset = True
+lvl_num=0
 
 while True:
-    while not reset:
+    sense.clear()
+    level=levels[lvl_num]
+    b = level["ball"]
+    g = level["goal"]
+    w = level["wall"]
+    draw_wall(w)
+    draw_goal(g)
+    playing=True
+    while playing:
         b = draw_ball(b)
         hit_goal = check_goal(b, g)
         hit_wall = check_wall(b, w)
-        if hit_goal or hit_wall:
-            reset = True
-    sense.clear()
-    draw_wall(w)
-    draw_goal(g)
-    b = (7,7)
-    reset = False
-
+        if hit_goal:
+            lvl_num = lvl_num + 1
+            playing=False
+        if hit_wall:
+            lvl_num = 0
+            playing=False
